ec73fe106335644d5ac8ffecfc3c5d4a3c4c2826
Ramazan <romka1501@gmail.com>
2023-07-11 22:24:29 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"üse strict"
+
+// const result = confirm('Are you here?');
+// console.log(result)
+
+// const answer = +prompt ('Вам есть 18?', '18');
+// console.log(answer + 5);
+const answers = [];
+
+answers[0] = prompt('Ваше имя?', '');
+answers[1] = prompt('Ваша фамилия?', '');
+answers[2] = prompt('Ваш возраст?', '');
+
+console.log(typeof(answers))
---
+++ js/script1.js
@@ -0,0 +1,25 @@
+"use strict"
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(2*4 == 8); 
+
+// const isChecked = false,
+//     isClose = false;
+
+// console.log(isChecked || !isClose)
